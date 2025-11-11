fb7930a357f57063aadf67592df4ba927dc6a95e
Tarun Raghav <tarunraghav@Taruns-MacBook-Pro-2.local>
2025-11-12 02:43:24 +0530

    init commit

---
+++ shared/src/commonMain/kotlin/com/tarun/mybeeapplication/Greeting.kt
@@ -0,0 +1,9 @@
+package com.tarun.mybeeapplication
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
